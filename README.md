afd04a0da376bc38ac4cf0fd7400a44cc72f6baf
alec <alec.hulce@gmail.com>
2016-08-12 20:03:01 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # watchs
-Use node or bash command to watch the next episode of your favorite show
+Use nodebash command to watch the next episode of your favorite show
+
+No node_module dependencies
+
+
+## Install
+
+
+## Commands
+
+
+## To Do
+- Move files in here
+- Finish README.md
+- Alternate install from git/bash if you don't want to use npm
+	- Script to install in .bashrc/.profile + make work with `npm install -g`
+
+
---
+++ index.js
@@ -0,0 +1,123 @@
+#!/usr/local/bin/node
+/*
+./watch.js mad-men -s2 -e1
+
+or add to ~/.bashrc:
+watch()(
+	PATH_TO_THIS_FILE/watch.js $@
+)
+
+*/
+
+var DEFAULT_WEB_APP = '/Applications/Google Chrome.app'
+,series = 'mad-men'
+,season = 1
+,episode = 1
+
+var http = require('http')
+,Url = require('url')
+,cp = require('child_process')
+,fs = require('fs')
+,argsMemFile = __dirname+'/.watch~'
+
+
+loadArgs(function(err,argv){
+	if (err) return console.error(err)
+	argv.forEach(function(arg){
+		if (arg.indexOf('-') != 0)
+			return series = arg
+		var argVal = arg.substr(2)
+		switch (arg.substr(1,1)) {
+			case 's': season = argVal; break
+			case 'e': episode = argVal; break
+		}
+	})
+	if (process.argv[2] == 'next') ++episode
+	console.log(['series: '+series,'season: '+season,'episode: '+episode].join('\n'))
+
+	
+	parseListingPage(function(err,data){
+		if (err == 404) {
+			++season
+			episode = 1
+			console.log('episode not found, trying season '+season+' episode '+episode)
+			return parseListingPage(parseEpisodesPage)
+		}
+		parseEpisodesPage(false,data)
+	})
+
+
+	function parseListingPage(cb){
+		console.log('searching for episode list...')
+		getUrl('http://watch8now.info/stream/'+series+'-s'+season+'e'+episode+'.html',cb)
+	}
+
+	function parseEpisodesPage(err,data){
+		if (err) return console.error(err)
+		var m = data.toString().match(/<a href="([^"]+)".+youwatch\.org/i)
+		if (!m) return console.error('unable to find episode link')
+		console.log('searching for video...')
+		getUrl(m[1],function(err,data){
+			if (err) return console.error(err)
+			var m = data.toString().match(/<iframe src="([^"]+)"/i)
+			if (!m) return console.error('unable to find episode iframe')
+			var cmd = 'open -a"'+DEFAULT_WEB_APP+'" "'+m[1]+'"'
+			//console.log(cmd)
+			cp.exec(cmd)
+			saveArgs([series,'-s'+season,'-e'+episode])
+		})
+	}
+})
+
+
+function getUrl(url,cb){
+	var urlp = Url.parse(url)
+	var requestOpts = {
+		hostname: urlp.hostname
+		,path: urlp.pathname
+		,port: urlp.port || 80
+		,method: 'GET'
+	};
+	var buf = new Buffer(0)
+	http.request(requestOpts,function(res){
+		if (res.statusCode == 404) return done('404')
+		res.on('data',function(chunk){
+			buf = Buffer.concat([buf,chunk])
+		})
+		.on('end',function(){
+			done(false,buf)
+		})
+		.on('error',done)
+	})
+	.on('error',done)
+	.end()
+	function done(err,data){
+		cb(err,data)
+		done = function(){}
+	}
+}
+
+function loadArgs(cb){
+	var args = process.argv.slice(2)
+	if (args[0] && args[0] != 'next') return process.nextTick(function(){ cb(false,args) })
+	fs.readFile(argsMemFile,function(err,data){
+		if (err && err.code != 'ENOENT') return cb(false,args);
+		try {
+			var savedArgs = JSON.parse(data)
+			if (!Array.isArray(savedArgs)) throw 'saved args not an array'
+			args = savedArgs
+		} catch (e) {
+			console.warn('unable to load saved args',e)
+		}
+		cb(false,args)
+	})
+}
+
+function saveArgs(args){
+	fs.writeFile(argsMemFile, JSON.stringify(args), function(err){
+		if (err) console.error(err)
+	})
+}
+
+
+
